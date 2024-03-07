11349de276e45f1aa8814149c673a16280b9e7c2
Justin Simmons <justin.simmons@airspacelink.com>
2024-03-07 15:45:13 -0500

    Add prelim README information

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # pgxmap
+
 Collection of helper functions to convert to and from pgtypes.
+
+[pgtype](https://github.com/jackc/pgtype) is the type system underlying the (pgx)[https://github.com/jackc/pgx] PostgreSQL driver. 
+
+I have created this off of past experience with the native [go database/sql](https://pkg.go.dev/database/sql) drivers. I have yet to determine how necessary this repo is. If it proves to be useless I will delete this is very much in Beta..
 
 ## License
 
--- uuid.go
+++ uuid.go
@@ -1,3 +1,10 @@
+// Copyright 2024 Justin Simmons.
+//
+// This file is part of pgxmap.
+// pgxmap is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+// pgxmap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License along with pgxmap. If not, see <https://www.gnu.org/licenses/>.
+
 package pgxmap
 
 import (
